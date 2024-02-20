081609b0c1fb30f5b0990813e90172c5468257cf
Anuj Dubey <danuj828@gmail.com>
2024-02-20 18:45:56 +0530

    Create a class based Component of About Section.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,19 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-    </div>
-  );
-};
+import React, { Component } from "react";
+import UserClass from "./UserClass";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <UserClass name={"first - class"} location={"New Delhi"} />
+      </div>
+    );
+  }
+}
 
 export default About;
